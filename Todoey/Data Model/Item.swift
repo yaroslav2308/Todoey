24737c05fba7616a79f25e8be53f29e188c6a456
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2021-11-18 17:44:00 +0300

    Created ItemModel and used it

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Yaroslav Monastyrev on 18.11.2021.
+//
+
+import Foundation
+
+class Item {
+    var title: String = "New Item"
+    var done: Bool = false
+}
